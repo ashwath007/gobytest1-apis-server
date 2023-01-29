0f0dc2ff247114b0f2845b777fc4924b4389b668
vicky <srivicky2000@gmail.com>
2023-01-29 17:32:33 +0530

    working on: all business

--- routes/business.js
+++ routes/business.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const route = express.Router();
 
-const { createBusiness, getBusinessByID, getBusinessUserProfileLink, deleteBusiness } = require('../controllers/business');
+const { createBusiness, getBusinessByID, getBusinessUserProfileLink, deleteBusiness, editBusiness, getAllBusiness } = require('../controllers/business');
 
 // ?? Post Business
 route.post('/create/business', createBusiness);
-route.delete('/delete/business', deleteBusiness);
+route.post('/edit/business/:bid', editBusiness);
+route.delete('/delete/business/:bid', deleteBusiness);
 // ** Post Get Business
 route.get('/get/a/business/:bid', getBusinessByID);
+route.get('/get/all/business', getAllBusiness);
 route.get('/get/a/business/user/link/:linkId', getBusinessUserProfileLink);
 
 
